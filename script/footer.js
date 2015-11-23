27b904155eb607131786c4f0e44c7db2e2a6bbfe
peter <life0001@126.com>
2015-11-23 13:00:30 +0800

    Update footer.js

--- script/footer.js
+++ script/footer.js
@@ -8,7 +8,7 @@ $(function(){
             document.addEventListener('touchend',touch, false);    // touchend: 当手指从屏幕上离开的时候触发。
             document.addEventListener('touchmove',touch, false);   //touchmove ：当手指在屏幕上滑动的时候连续地触发。
 
-            var startY,endY,startX,endX,$table=$('footer table');
+            var startY,endY,startX,endX,$table=$('footer div.main_footer_navi');
             function touch (event){
                 var event = event || window.event;
                 switch(event.type){
@@ -26,11 +26,25 @@ $(function(){
                             $table.show();
                         }
                         break;
-                    case "touchmove":
-                        event.preventDefault();
-                        break;
+                    //case "touchmove":
+                    //    event.preventDefault();
+                    //    break;
                 }
             }
-        })()
+        }),
+        // 用下面这个，上面那个无效
+        scroll:(function(){
+            var scroll,scrolltop=document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop,$table=$('footer div.main_footer_navi');
+            $(window).scroll(function(){
+                scrolltop=document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+                if(scroll<scrolltop){
+                    $table.hide();
+                    setTimeout(function(){
+                        $table.fadeIn();
+                    },2000)
+                }
+                scroll=scrolltop;
+            });
+    })()
     }
 });
